2b97e01090ee07668c586b0d3b4441eedcf6bebc
Victory Oluwaseun Johnson <johnsonvictory2702@gmail.com>
2025-02-08 05:36:12 +0100

    edited

--- week-2-projects/Victory-O-Johnson/Week2A/Smart_Contract/hardhat.config.js
+++ week-2-projects/Victory-O-Johnson/Week2A/Smart_Contract/hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const METIS_SEPOLIA_RPC_URL = process.env.METIS_SEPOLIA_RPC_URL;
-const METIS_MAINET_RPC_URL = process.env.METIS_MAINET_RPC_URL; // Replace this with the Metis Sepolia RPC URL
+// Replace this with the Metis Sepolia RPC URL
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -15,10 +15,6 @@ module.exports = {
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
       chainId: 59902,
     },
-    metisMainet: {
-      url: METIS_MAINET_RPC_URL,
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
-      chainId: 1008,
-    },
-  }
-};
+  },
+}
+
